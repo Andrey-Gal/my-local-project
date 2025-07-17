f93148527203aa09f04dba50f1ff027b352f1f44
Andrey Galkin <andreygalkin85@gmail.com>
2025-07-17 11:35:41 +0300

    Обновление кнопки

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const buttonNode = document.getElementById('button');
 let counter = 0;
 
-counter = counter + 1;
-buttonNode.innerText = counter;
-
 buttonNode.addEventListener('click', () => {
-    alert('hello');
+    counter = counter + 1;
+    buttonNode.innerText = counter;
 });
